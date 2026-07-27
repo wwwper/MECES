9f0235b1d1a9c8ceab1a4b62f4e012bd242ed0e2
wwwper <113246049+wwwper@users.noreply.github.com>
2026-07-27 23:17:41 +0800

    Update infer_meca.sh

--- MPF-LLM/scripts/infer_meca.sh
+++ MPF-LLM/scripts/infer_meca.sh
@@ -2,10 +2,10 @@
 # 从仓库根目录运行:  bash scripts/infer_meca.sh
 set -ex
 
-export CUDA_VISIBLE_DEVICES=6
+export CUDA_VISIBLE_DEVICES=1
 
 BASE_MODEL_PATH="/data/wjk/chatglm3-6b-base"
-LORA_PATH="./results/model_checkpoint/checkpoint-1581"   # 这里选择了最优的epoch=3保存的checkpoint
+LORA_PATH="./results/model_checkpoint/checkpoint-1581"   # 这里选择了epoch=3保存的checkpoint
 VIDEO_FEATURES="./mpf_llm/dataset/multimodal_features/video_features.pt"
 AUDIO_FEATURES="./mpf_llm/dataset/multimodal_features/audio_features.pt"
 TEST_FILE="./mpf_llm/dataset/finetune_prompt_data/MECES_multimodal_test_finetune.json"
