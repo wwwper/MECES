b6c471db152bc325c7caf36c33ed46b17b093fcd
wwwper <113246049+wwwper@users.noreply.github.com>
2026-07-27 23:10:01 +0800

    Update Sentence_Bert.py

--- MPF-LLM/Evaluate/Sentence_Bert.py
+++ MPF-LLM/Evaluate/Sentence_Bert.py
@@ -11,7 +11,7 @@ print("模型加载完成。")
 
 pred =[]
 # 请确保这里的路径是正确的
-with open('./results/pred/', 'r', encoding='utf-8') as file:
+with open('../results/pred/MECES_pred.json', 'r', encoding='utf-8') as file:
     pred_data = json.load(file)
     for i in range(0, len(pred_data)):
         for j in range(0, len(pred_data[i]["dialog"])):
@@ -19,7 +19,7 @@ with open('./results/pred/', 'r', encoding='utf-8') as file:
                 pred_data.append(pred_data[i]["dialog"][j]["Cause_summary"][0])
 
 ref =[]
-with open('./mpf_llm/dataset/MECESD_test.json', 'r', encoding='utf-8') as file:
+with open('../mpf_llm/dataset/MECESD_test.json', 'r', encoding='utf-8') as file:
     ref_data = json.load(file)
     for i in range(0, len(ref_data)):
         for j in range(0, len(ref_data[i]["dialog"])):
